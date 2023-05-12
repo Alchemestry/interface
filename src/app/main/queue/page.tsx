3c79011bebdb772011af658e210bdd080a0f91b3
Ilya Kubariev <ilya.kubariev@redduck.io>
2023-05-13 02:53:48 +0300

    feat: next update, updated page structure

--- src/app/main/queue/page.tsx
+++ src/app/main/queue/page.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { ProgressTable } from '@/components/ProgressTable';
 
 const QueuePage = async () => {
-  return <ProgressTable />;
+  return (
+    <div className="flex h-full w-full flex-col">
+      <ProgressTable />
+    </div>
+  );
 };
 
 export default QueuePage;
