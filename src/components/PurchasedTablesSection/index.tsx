a8295be1f3e09a918836d5d8b7d009517580fa86
kostyamospan <kostya04042001@gmail.com>
2023-05-16 21:53:26 +0300

    chore: gear icon purchased tables

--- src/components/PurchasedTablesSection/index.tsx
+++ src/components/PurchasedTablesSection/index.tsx
@@ -8,6 +8,7 @@ import { Title } from '../Title';
 import ProgressTableCardLevel1 from '@/../public/images/progress-table-card-level-1.png';
 import ProgressTableCardLevel2 from '@/../public/images/progress-table-card-level-2.png';
 import ProgressTableCardLevel3 from '@/../public/images/progress-table-card-level-3.png';
+import { GearIcon } from '../icons/GearIcon';
 
 export const PurchasedTablesSection = () => {
   const progressTableCards = [
@@ -36,7 +37,7 @@ export const PurchasedTablesSection = () => {
       <div>
         <div className='flex flex-row items-center'>
           <Title>Purchased Tables</Title>
-          <BNBIcon className='ml-5 mb-2' size={'1.7rem'}/>
+          <GearIcon className='ml-5 mb-2' size={'1.7rem'}/>
         </div>
         <div className="ml-9 mt-9 flex flex-wrap gap-x-9 gap-y-9">
           {progressTableCards.map(({ title, image, amount, price }) => (
