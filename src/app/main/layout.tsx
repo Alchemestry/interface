a99e07c296894c14d0bcae83c650bbcd7cbbbac0
Romazes <romazes20@gmail.com>
2023-05-17 20:07:14 +0300

    fix: layout padding left

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from '@/components/Navbar';
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <ClientProviders>
-      <main className="left-0 top-0 flex h-full w-full flex-col overflow-auto bg-main px-10 lg:pl-24">
+      <main className="left-0 top-0 flex h-full w-full flex-col overflow-auto bg-main px-5 md:px-10">
         <Navbar />
         {children}
       </main>
