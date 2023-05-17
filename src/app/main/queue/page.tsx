6635ab5cf6132c066bab30f318e77437e4a9503f
Romazes <romazes20@gmail.com>
2023-05-17 20:07:47 +0300

    fix: Pool component

--- src/app/main/queue/page.tsx
+++ src/app/main/queue/page.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { BuyTables } from '@/components/BuyTables';
 import Pool from '@/components/Pool';
-import { ProgressTable } from '@/components/ProgressTable';
 
 const QueuePage = async () => {
   return (
-    <div className="flex h-full w-full flex-col">
-      <ProgressTable />
-      <BuyTables />
+    <>
       <Pool />
-    </div>
+    </>
   );
 };
 
