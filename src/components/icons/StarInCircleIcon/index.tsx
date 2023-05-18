3eb6c35090cdb68746b3fd55c711ce568ea2218d
Romazes <romazes20@gmail.com>
2023-05-18 20:05:36 +0300

    fix: star in circle svg

--- src/components/icons/StarInCircleIcon/index.tsx
+++ src/components/icons/StarInCircleIcon/index.tsx
@@ -14,7 +14,7 @@ export const StarInCircleIcon: FC<StarInCircleIconProps> = (props) => {
       viewBox="0 0 37 37"
       fill="none"
     >
-      <circle cx="18.5" cy="18.5" r="10" stroke="#F4E6B0" />
+      <circle cx="18.5" cy="18.5" r="10" stroke="#F4E6B0" strokeWidth="1"/>
       <path
         d="M18.5 0L20.593 16.407L37 18.5L20.593 20.593L18.5 37L16.407 20.593L0 18.5L16.407 16.407L18.5 0Z"
         fill="#F4E6B0"
