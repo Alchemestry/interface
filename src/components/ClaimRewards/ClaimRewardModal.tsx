c29eee11be8a843eb4b32e8d6fadbcbfb09b64f1
Romazes <romazes20@gmail.com>
2023-06-28 18:47:31 +0300

    feat: complete claim reward modal

--- src/components/ClaimRewards/ClaimRewardModal.tsx
+++ src/components/ClaimRewards/ClaimRewardModal.tsx
@@ -13,35 +13,48 @@ export const ClaimRewardModal = () => {
 
   const userHasReward = true;
 
+  const Row = (row1: string, row2: string, row3: string) => {
+    return (
+      <div className="mb-5 flex text-2xl md:w-[605px]">
+        <div className="basis-2/4">{row1}</div>
+        <div className="basis-2/5 text-end">{row2}</div>
+        <div className="ml-2.5">{row3}</div>
+      </div>
+    );
+  };
+
   return (
     <>
       <BaseModal isOpen={isOpen}>
         <GradientDiv>
-          <div className="bg-primary pl-9 pr-20">
+          <div className="bg-primary pl-9 pr-9">
             <div className="pt-9">
               <Title>Claim Rewards</Title>
             </div>
             {userHasReward ? (
-              <div className="grid ">
-                <div className="flex">
-                  <div>Referrals</div>
-                  <div>4</div>
-                  <div>BNB</div>
+              <div className="ml-9 mt-6 grid">
+                {Row('Referrals', '4', 'BNB')}
+                {Row('Random Pool wins', '100000', 'BNB')}
+                {Row('Queue Pool wins', '234', 'BNB')}
+                <div className="mt-7 flex text-3xl">
+                  <div>Total:</div>
+                  <div className="ml-5 min-w-[5rem]">132310</div>
+                  <div className="ml-1.5">BNB</div>
                 </div>
-                <div className="flex">
-                  <div>Random Pool wins</div>
-                  <div>100000</div>
-                  <div>BNB</div>
-                </div>
-                <div className="flex">
-                  <div>Queue Pool wins</div>
-                  <div>234</div>
-                  <div>BNB</div>
-                </div>
-                <div className="text-[2.625rem]">
-                  <GradientButton className="min-w-[210px] rounded-full text-secondary">
-                    Claim
-                  </GradientButton>
+                <div className="mb-9 mt-12 flex justify-between">
+                  <div className="self-center text-2xl">
+                    <button
+                      onClick={onClose}
+                      className="border-b-2 border-dashed border-primary"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                  <div className=" text-[2.625rem]">
+                    <GradientButton className="min-w-[210px] rounded-full text-secondary">
+                      Claim
+                    </GradientButton>
+                  </div>
                 </div>
               </div>
             ) : (
